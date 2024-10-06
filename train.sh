c795b4fbf80e2dd72c03b776599ad69542620543
Daniel Vega-Myhre <dvegamyhre@gmail.com>
2024-10-05 20:38:45 -0700

    add train bash script

---
+++ train.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 train.py --dataset-file=data/data.csv --device=mps --epochs 1 --learning-rate .01 --save-checkpoint checkpoints/chkpt.pt --eval-interval 1 --checkpoint-interval 1 --batch-size 2 --plot-learning-curves
