953a0cf77de5d70e625440e72dbeae003850203c
Daniel Vega-Myhre <dvegamyhre@gmail.com>
2024-10-15 19:34:38 -0700

    add perf analysis script for distributed training

---
+++ dist-perf-analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+torchrun --nproc_per_node=2 --master_port=12345 translate_ai/train.py \
+    --dataset-file data/english-spanish.csv \
+    --device cuda \
+    --learning-rate .001 \
+    --batch-size 32 \
+    --num-layers 2 \
+    --embed-dim 128 \
+    --d-model 128 \
+    --ffwd-dim 512 \
+    --seq-len 128 \
+    --max-output-tokens 128 \
+    --hardware-peak-tflops 19.05 \
+    --distributed \
+    --estimate-mfu
