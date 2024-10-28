43dbd53c18ed591580ee3d203fd944b5971bcf0f
Daniel Vega-Myhre <dvegamyhre@gmail.com>
2024-10-27 17:39:36 -0700

    add group norm and learnable scalar lambda into attention layers

--- translate_ai/attention.py
+++ translate_ai/attention.py
@@ -5,21 +5,33 @@ from torch import nn
 from math import sqrt
 
 class DifferentialMultiHeadCrossAttention(nn.Module):#
-    def __init__(self, num_heads: int, embed_dim: int, d_model: int, dropout: float = 0.1):
+    def __init__(self, num_heads: int, embed_dim: int, d_model: int, dropout: float = 0.1, lambda_init: float = 0.8):
         super(DifferentialMultiHeadCrossAttention, self).__init__()
         assert d_model % num_heads == 0
+        self.num_heads = num_heads
+        self.embed_dim = embed_dim
+        self.d_model = d_model
         head_dim = d_model // num_heads
         self.heads = nn.ModuleList([
-            DifferentialCrossAttentionHead(embed_dim, head_dim)
+            DifferentialCrossAttentionHead(embed_dim, head_dim, lambda_init=lambda_init)
             for _ in range(num_heads)
         ])
         self.linear = nn.Linear(d_model, embed_dim)
         self.dropout = nn.Dropout(dropout)
+        self.group_norm = nn.GroupNorm(num_groups=num_heads, num_channels=d_model)
 
     def forward(self, x: torch.Tensor, encoder_out: torch.Tensor) -> torch.Tensor:
         # each head: B,T,C -> B,T,head_dim
         # after concat: B,T,d_model
         x = torch.cat([head(x, encoder_out) for head in self.heads], dim=-1)
+
+        # group norm expects dims to be (batch, channels, seq len), 
+        # so permute B,T,C -> B,C,T
+        x = x.permute(0, 2, 1)
+        x = self.group_norm(x)
+
+        # reshape B,C,T back to B,T,C
+        x = x.permute(0, 2, 1)
         x = self.linear(x)  # B,T,d_model -> B,T,C
         x = self.dropout(x)
         return x
@@ -28,7 +40,8 @@ class DifferentialCrossAttentionHead(nn.Module):
     def __init__(self, 
                  embed_dim: int, 
                  head_dim: int,
-                 dropout: float = 0.1):
+                 dropout: float = 0.1,
+                 lambda_init: float = 0.8):
         super(DifferentialCrossAttentionHead, self).__init__()
         self.Q1 = nn.Linear(embed_dim, head_dim)
         self.Q2 = nn.Linear(embed_dim, head_dim)
@@ -36,6 +49,7 @@ class DifferentialCrossAttentionHead(nn.Module):
         self.K2 = nn.Linear(embed_dim, head_dim)
         self.V = nn.Linear(embed_dim, head_dim)
         self.dropout = nn.Dropout(dropout)
+        self._lambda = nn.Parameter(torch.tensor(lambda_init))
     
     def forward(self, x: torch.Tensor, encoder_out: torch.Tensor) -> torch.Tensor:
         # queries come from previous decoder layer
@@ -58,7 +72,7 @@ class DifferentialCrossAttentionHead(nn.Module):
         weighted_scores2 = torch.softmax(scores2, dim=-1)
 
         # subtract weighted scores to reduce noise
-        weighted_scores = weighted_scores1 - weighted_scores2
+        weighted_scores = weighted_scores1 - self._lambda * weighted_scores2
         weighted_scores = self.dropout(weighted_scores)
 
         # B,T,C @ C,head_dim -> B,T,head_dim
@@ -73,22 +87,36 @@ class DifferentialMultiHeadSelfAttention(nn.Module):
                  num_heads: int,
                  embed_dim: int,
                  d_model: int,
-                 dropout: float = 0.1):
+                 dropout: float = 0.1,
+                 lambda_init: float = 0.8):
         super(DifferentialMultiHeadSelfAttention, self).__init__()
         assert d_model % num_heads == 0
+        self.num_heads = num_heads
+        self.embed_dim = embed_dim
+        self.d_model = d_model
         head_dim = d_model // num_heads
         self.heads = nn.ModuleList([
-            DifferentialSelfAttentionHead(embed_dim, head_dim)
+            DifferentialSelfAttentionHead(embed_dim, head_dim, lambda_init=lambda_init)
             for _ in range(num_heads)
         ])
         self.linear = nn.Linear(d_model, embed_dim)
         self.dropout = nn.Dropout(dropout)
+        self.group_norm = nn.GroupNorm(num_groups=num_heads, num_channels=d_model)
 
     def forward(self, x: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
         # head(x) => (B,T,head_size)
         # concat them all along head_size dim -> (B,T,H)
         # since H = head_size * num_heads 
         x = torch.cat([head(x, mask) for head in self.heads], dim=-1)
+        
+        # group norm expects dims to be (batch, channels, seq len), 
+        # so permute B,T,C -> B,C,T
+        x = x.permute(0, 2, 1)
+        x = self.group_norm(x)
+        
+        # reshape B,C,T back to B,T,C
+        x = x.permute(0, 2, 1)
+
         # project to embed dimension to enable residual (i.e. x + mha(x))
         x = self.linear(x)  #  (B,T,H) -> B,T,C
         x = self.dropout(x)
@@ -101,7 +129,8 @@ class DifferentialSelfAttentionHead(nn.Module):
     def __init__(self,
                  embed_dim: int = 128,
                  head_dim: int = 128,
-                 dropout: float = 0.1):
+                 dropout: float = 0.1,
+                 lambda_init: float = 0.8):
         super(DifferentialSelfAttentionHead, self).__init__()
         self.Q1 = nn.Linear(embed_dim, head_dim)
         self.Q2 = nn.Linear(embed_dim, head_dim)
@@ -109,6 +138,7 @@ class DifferentialSelfAttentionHead(nn.Module):
         self.K2 = nn.Linear(embed_dim, head_dim)
         self.V = nn.Linear(embed_dim, head_dim)
         self.dropout = nn.Dropout(dropout)
+        self._lambda = nn.Parameter(torch.tensor(lambda_init))
     
     def forward(self, x: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
         # (B,T,C) @ (C,head_dim) = (B,T,head_dim)
@@ -134,7 +164,7 @@ class DifferentialSelfAttentionHead(nn.Module):
         weighted_scores2 = torch.softmax(scores2, dim=-1)
         
         # subtract weighted scores to reduce noise in attention scores
-        weighted_scores = weighted_scores1 - weighted_scores2
+        weighted_scores = weighted_scores1 - self._lambda * weighted_scores2
 
         # apply dropout
         weighted_scores = self.dropout(weighted_scores)
